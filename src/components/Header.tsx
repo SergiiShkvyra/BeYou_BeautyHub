8fce4cb47be8e407b3d45aef1dd5b8596f2cd5ad
SergiiShkvyra <65265793+SergiiShkvyra@users.noreply.github.com>
2025-08-08 19:39:01 -0400

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -257,7 +257,7 @@ const Header = () => {
             <div className="text-4xl sm:text-5xl lg:text-6xl xl:text-8xl tracking-wider leading-none self-end">
               BE YOU
             </div>
-            <div className="text-base sm:text-lg lg:text-xl tracking-wide opacity-90 self-end">
+            <div className="text-base sm:text-lg lg:text-xl tracking-wide opacity-100 self-end">
               BeautyHub
             </div>
           </div>
