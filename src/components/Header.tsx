22c28fa3efc3cca8b4d5b8afa69d55ff3578fcc9
SergiiShkvyra <65265793+SergiiShkvyra@users.noreply.github.com>
2025-08-24 22:17:19 -0400

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -265,7 +265,6 @@ const Header = () => {
           >
             {/* Logo Icon */}
             <div className="relative">
-              <Sparkles className="h-8 w-8 sm:h-10 sm:w-10 text-olive group-hover:text-olive/80 transition-colors duration-200" />
               <div className="absolute -top-1 -right-1 w-3 h-3 bg-olive rounded-full opacity-60 group-hover:opacity-80 transition-opacity duration-200"></div>
             </div>
             
