049c3956bfc2ee95aef89faa7f53da6af94a3040
SergiiShkvyra <65265793+SergiiShkvyra@users.noreply.github.com>
2025-08-26 22:39:56 -0400

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -52,7 +52,7 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-warm">
       {/* Main footer content */}
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-16 py-10">
+      <div className="max-w-7xl mx-auto px-4 sm:px-16 lg:px-16 py-10">
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
           {/* Company Info */}
           <div className="text-center md:text-left">
