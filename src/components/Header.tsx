daf597e33568220d969a0c3c1c6def0bf49cf9c6
SergiiShkvyra <65265793+SergiiShkvyra@users.noreply.github.com>
2025-08-29 18:17:42 -0400

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -340,7 +340,7 @@ const Header = () => {
                 <span>YOU</span>
               </span>
               <span 
-                className="ml-4 text-2xl sm:text-3xl lg:text-2xl xl:text-5xl font-light tracking-wide"
+                className="ml-4 text-2xl sm:text-3xl lg:text-4xl xl:text-5xl font-light tracking-wide"
                 style={{
                   opacity: beautyHubOpacity,
                   transition: 'opacity 0.3s ease-out'
